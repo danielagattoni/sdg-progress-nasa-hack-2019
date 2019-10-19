8f4158aa99640d79898715c77633d042b33c46af
Daniela Gattoni <daniela.gattoni@localz.co>
2019-10-19 13:08:10 +1100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,185 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  ThemeProvider,
+  Button,
+  styled,
+  css,
+  FieldSet,
+  InputField,
+  Flex,
+  Box,
+  Paragraph
+} from 'fannypack';
+import 'typeface-roboto-mono';
+
+const theme = {
+  global: {
+    fontFamily: 'Roboto Mono',
+    fontSize: 20,
+    base: css`
+      html,
+      body {
+        color: #000;
+        overflow-x: inherit;
+      }
+    `
+  },
+  palette: {
+    primary: 'blue'
+  },
+  layout: {
+    mobileBreakpoint: 520,
+    tabletBreakpoint: 960
+  },
+  Button: {
+    disabled: css`
+      opacity: 0.2
+    `
+  },
+  Text: css`
+    font-weight: 300;
+    color: #000;
+  `
+}
+
+const Container = styled.div`
+  text-align: center;
+  display: grid;
+  grid-gap: 0.5rem;
+  grid-template-columns: 1fr;
+
+  @media screen and (min-width: 500px) {
+    grid-template-columns: 5rem 3fr 1fr 5rem;
+  }
+`
+
+const StyledHeader = styled.header`
+
+  @media screen and (min-width: 500px) {
+    grid-column-start: 1;
+    grid-column-end: -1;
+    grid-row-start: 2;
+  }
+`
+
+const StyledNav = styled.nav`
+  background-color: #fff;
+  border-bottom: 1px dashed #000;
+
+  @media screen and (min-width: 500px) {
+    grid-row: 1;
+    grid-column-start: 1;
+    grid-column-end: -1;
+  }
+`
+
+const StyledMain = styled.main`
+  background-color: #fff;
+  border: 2px dashed #000;
+
+  @media screen and (min-width: 500px) {
+    grid-row: 3 / 4;
+    grid-column: 2;
+  }
+`
+
+const StyledAside = styled.aside`
+  background-color: #fff;  
+  border: 2px dashed #000;
+  display: flex;
+
+  @media screen and (min-width: 500px) {
+    grid-row: 3 / span 2;
+    grid-column: 3;
+  }
+`
+
+const StyledFooter = styled.footer`
+  border-top: 1px dashed #000;
+
+  @media screen and (min-width: 500px) {
+    grid-column-start: 1;
+    grid-column-end: -1;
+  }
+`
+
+const StyledMenu = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  && li {
+    justify-self: space-between;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <StyledNav>
+          <StyledMenu>
+            <li>SDG Progress</li>
+            <li>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://2019.spaceappschallenge.org/challenges/living-our-world/smash-your-sdgs/teams/team-amulen/members">
+                  The Team
+              </a>
+            </li>
+            <li>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://2019.spaceappschallenge.org/challenges/living-our-world/smash-your-sdgs/teams/team-amulen/project">
+                  About the project
+              </a>
+            </li>
+
+          </StyledMenu>
+        </StyledNav>
+        <StyledHeader>
+          <Flex alignItems="center" column padding="major-2">
+            <h1>SDG Progress</h1>
+            <Paragraph style={{
+                width: "60%",
+            }}>
+              A tool that simulates and visualizes the progress of a United Nation’s Sustainable development Goal, one chart at a time. 
+            </Paragraph>
+          </Flex>
+        </StyledHeader>
+        <StyledMain>
+         <Box width="100%" padding="major-3">
+            Charts goes here
+          </Box>
+        </StyledMain>
+        <StyledAside>
+          <Box width="100%" padding="major-3">
+            <Paragraph use="h2" isSubHeading>Add a Government Budget (?)</Paragraph>
+            <FieldSet>
+              <InputField name="total" label="label" />
+              <InputField name="total" label="label" />
+              <InputField name="total" label="label" />
+              <InputField name="total" label="label" />
+              <InputField name="total" label="label" />
+              <Button width="100%">Submit</Button>
+
+            </FieldSet>
+          
+            </Box>
+          </StyledAside>
+        <StyledFooter>
+          <Flex column padding="major-3">
+          <Paragraph>
+            Designed, built, and maintained by Daniela, Melissa and Craig
+          </Paragraph>
+          <Paragraph>
+            NASA Space Apps 2019
+          </Paragraph>
+          </Flex>
+        </StyledFooter>
+      </Container>
+    </ThemeProvider>
+
   );
 }
 
