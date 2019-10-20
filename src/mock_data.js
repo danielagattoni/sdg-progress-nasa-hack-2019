f33e8933c680dc64b9c973a5d2fd21188da94dca
melissa gattoni <meligatt@gmail.com>
2019-10-20 14:30:32 +1100

    wip bar chart

---
+++ src/mock_data.js
@@ -0,0 +1,12 @@
+const mocked_data = [
+  {
+    target: 'Target 3.1', indicator1: 30,
+  },
+  {
+    target: 'Target 3.2', indicator1: 20, indicator2: 10
+  },
+  {
+    target: 'Target 3.7', indicator1: 100,
+  },
+];
+export default mocked_data;
